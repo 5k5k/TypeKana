c9d5f8f75d598118f6256ca07fff5359144edc19
5k5k <5k5k@sina.com>
2015-02-08 20:33:43 +0800

    v0.0.3

--- js/main.js
+++ js/main.js
@@ -6,47 +6,44 @@ function init() {
     resetCanvasSize();
     resetKanaWidth();
 
-    containers = new Array();
-    containers[0] = createKana();
-    stage.addChild(containers[0]);
+    kanas = new Array();
+    kana = new Kana("a", 100);
+    stage.addChild(kana.getInstance());
     stage.update();
     createjs.Ticker.on("tick", tick);
+    createjs.Ticker.framerate = fps;
 
+    window.addEventListener("keydown", function (event) {
+        event = event || window.event;
+        if (event.keyCode === kana.keyCode) {
+//            man.jump();
+            console.log("a");
+            kana.destroy();
+        }
+    });
 }
 
 function tick() {
-    containers[0].y += 3;
-    console.log("y "+    containers[0].y);
+    currentDate = new Date();
+
+//    if (currentDate - lastDate > appearTime) {
+//        kana = new Kana("a", 100);
+//        kanas.addChild(kana);
+//        stage.addChild(kana.getInstance());
+//    }
+    if (kana.getInstance() != null) {
+//        kana.setY(kana.getY() + 3);
+        kana.goNext();
+    }
+
+//    console.log("y " + date.getTime());
     stage.update();
-}
-
-
-function createKana() {
-    var container = new createjs.Container();
-//    var circle =new createjs.shape().graphics.f("green").dc(0,0,50);
-//    circle.x=200;
-//    circle.y=200;
-    var circle = new createjs.Shape();
-    circle.graphics.beginFill("red").drawCircle(0, 0, kana_r);
-    circle.x = kana_r;
-    circle.y = kana_r;
-
-    var text = new createjs.Text("a", "bold " + kana_d + "px Arial", "#ff7700");
-    container.addChild(circle);
-    container.addChild(text);
-
-    container.x = 100;
-    container.y = -kana_d;
-    return container;
 }
 
 window.onresize = function () {
     resetCanvasSize();
     resetKanaWidth();
-
     stage.update();
-
-//    console.log("a C_W " + C_W + " C_H" + C_H);
 }
 
 function resetCanvasSize() {
@@ -61,5 +58,55 @@ function resetKanaWidth() {
     kana_r = kana_d / 2;
 }
 
-var stage, canvasW, canvasH, kana_d, kana_r, containers;
+//Kana类
+function Kana(name, x) {
+    this.name = name;
+    this.x = x;
+    this.y = -kana_d;
+    this.speed = 10;//经过几秒会落到底部
+    this.perStepLength = canvasH / this.speed / fps;
+//        getPerStepLength();
+    this.keyCode = 65;
+    this.getInstance = function () {
+        return container;
+    }
+//    this.setY = function (y) {
+//        this.y = y;
+//        container.y = y;
+//    }
+//    this.getY = function () {
+//        return container.y;
+//    }
+    this.destroy = function () {
+        container.removeAllChildren();
+        container = null;
+    }
+    this.goNext = function () {
+//        console.log("a " + this.perStepLength)
+        this.y += this.perStepLength;
+        container.y = this.y;
+    }
+
+
+    var container = new createjs.Container();
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("red").drawCircle(0, 0, kana_r);
+    circle.x = kana_r;
+    circle.y = kana_r;
+    var text = new createjs.Text(name, "bold " + kana_d + "px Arial", "#ff7700");
+    container.addChild(circle);
+    container.addChild(text);
+    container.x = x;
+    container.y = this.y;
+
+//    function getPerStepLength() {
+//        console.log("canvasH "+canvasH+" "+this.speed+" "+fps);
+//        return  canvasH / this.speed / fps;
+//    }
+}
+
+var stage, canvasW, canvasH, kana_d, kana_r, kanas;
 var amount = 10; //一屏幕宽能同时放多少个假名
+var fps = 30;
+var appearTime = 1000;//ms
+var currentDate, lastDate = 0;
