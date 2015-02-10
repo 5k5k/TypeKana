ce058724c73069142665543744ae76c7b95d8b9c
5k5k <5k5k@sina.com>
2015-02-10 13:39:22 +0800

    Revert "v0.1.0"
    This reverts commit b77f89208e1a18b99b5800929be44c874416a8d2.

--- js/main.js
+++ js/main.js
@@ -21,13 +21,14 @@ function init() {
 //        console.log(keycodes);
         for (var i = 0; i < kanas.length; i++) {
             for (var j = 0; j < kanas[i].keyCodes.length; j++) {
-//                var k1 = kanas[i].keyCodes[j];
-//                var k2 = keycodes.slice(0, kanas[i].keyCodes[j].length).reverse();
-////                console.log("k1 " + );
-////                console.log("k2 " +);
+                var k1 = kanas[i].keyCodes[j];
+                var k2 = keycodes.slice(0, kanas[i].keyCodes[j].length).reverse();
+//                console.log("k1 " + );
+//                console.log("k2 " +);
                 var equal = true;
-                for (var k = 0; k < kanas[i].keyCodes[j].length; k++) {
-                    if (kanas[i].keyCodes[j][k] != keycodes.slice(0, kanas[i].keyCodes[j].length).reverse()[k]) {
+
+                for (var k = 0; k < k1.length; k++) {
+                    if (k1[k] != k2[k]) {
                         equal = false;
                         break;
                     }
@@ -48,29 +49,21 @@ function init() {
         great.text = greatText + greatNum;
     });
 
-    //为了解决最小化不走onblur和onfocus，不能删除
-    document.addEventListener('msvisibilitychange', function () {
-//        console.log(document.msVisibilityState);
-    });
+//    console.log(Hiragana.keys[0][0]);
+//    console.log(Hiragana.keys.length);
+//    console.log(Hiragana.keys[7]);
+//    console.log(Hiragana.keys[9]);
+//    Hiragana = new Map();
+//    Hiragana.set("aa",["bb","cc"]);
 
-    document.addEventListener('mozvisibilitychange', function () {
-//        console.log(document.mozVisibilityState);
-    });
-
-    document.addEventListener('webkitvisibilitychange', function () {
-//        console.log(document.webkitVisibilityState);
-    });
+//    console.log(    Hiragana.get("aa")[0]);
 }
 
-
-function tick(event) {
-    if (event.paused) {
-        return;
-    }
+function tick() {
     currentDate = new Date();
 //    var kana = null;
-    if (currentDate - lastDate - createjs.Ticker.getTime(false) + createjs.Ticker.getTime(true) > appearTime) {
-        lastDate = currentDate - createjs.Ticker.getTime(false) + createjs.Ticker.getTime(true);
+    if (currentDate - lastDate > appearTime) {
+        lastDate = currentDate;
 //        console.log(hiragana[getRandom(hiragana.length)]);
 
 //        console.log(canvasH + " " + kana_d);
@@ -120,45 +113,19 @@ function tick(event) {
 window.onresize = function () {
     resetCanvasSize();
     resetKanaWidth();
-    resetTexts();
     stage.update();
 };
 
-window.onblur = function (e) {
-    e = e || window.event;
-    if (window.ActiveXObject && /MSIE/.test(navigator.userAgent)) {  //IE
-        //如果 blur 事件是窗口内部的点击所产生，返回 false, 也就是说这是一个假的 blur
-        var x = e.clientX;
-        var y = e.clientY;
-        var w = document.body.clientWidth;
-        var h = document.body.clientHeight;
-
-        if (x >= 0 && x <= w && y >= 0 && y <= h) {
-            window.focus();
-            return false;
-        }
-    }
-
-//    var paused = !createjs.Ticker.getPaused();
-//    createjs.Ticker.setPaused(paused);
-    createjs.Ticker.paused = true;
-    console.log("true");
-}
-
-window.onfocus = function () {
-    createjs.Ticker.paused = false;
-    console.log("false");
-}
-
 function resetCanvasSize() {
-    document.getElementById("mCanvas").width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    document.getElementById("mCanvas").height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    document.getElementById("mCanvas").width = document.body.clientWidth;
+    document.getElementById("mCanvas").height = document.body.clientHeight/10*9;
+//    document.getElementById("mCanvas").height = document.body.clientHeight / 2;
     canvasW = stage.canvas.width;
     canvasH = stage.canvas.height;
 }
 
 function resetKanaWidth() {
-    kana_d = (canvasW > canvasH ? canvasH : canvasW) / amount;
+    kana_d = canvasW / amount;
     kana_r = kana_d / 2;
 }
 
@@ -166,12 +133,14 @@ function resetKanaWidth() {
 function Kana(name) {
     this.name = name;
     this.x = getRandom(canvasW - kana_d);
+//    this.x = x;
     this.y = -kana_d;
     this.speed = 10;//经过几秒会落到底部
     this.perStepLength = canvasH / this.speed / fps;
 //        getPerStepLength();
-    console.log("create " + name);
+    console.log(name);
     this.keyCodes = hiraganaMap.get(name);
+//    console.log(this.keyCodes);
 
     this.getInstance = function () {
         return container;
@@ -418,16 +387,6 @@ function initText() {
     miss.y = canvasH - keyTextSize;
     stage.addChild(miss);
 }
-
-function resetTexts() {
-    key.y = canvasH - keyTextSize;
-    great.y = canvasH - keyTextSize;
-    miss.y = canvasH - keyTextSize;
-    for (var i = 0; i < kanas.length; i++) {
-        kanas[i].getInstance().getChildAt(0).font = "bold " + kana_d + "px Arial";
-    }
-}
-
 function changeKeycodesToChars() {
     var str = "";
     for (var i = keycodes.length - 1; i >= 0; i--) {
@@ -448,8 +407,8 @@ var amount = 10; //一屏幕宽能同时放多少个假名
 var fps = 30;
 var appearTime = 10000;//ms
 var currentDate = 0, lastDate = 0;
-var state, menuScreen = 0, inGameScreen = 1, levelsScreen = 2, freedomScreen = 3, setScreen = 4, aboutScreen = 5;
 
+var hiraganaMap, hiragana;
 var keycodes;
 var key;
 var keyText = "key:";
@@ -459,5 +418,4 @@ var greatText = "hit:", missText = "miss:";
 var keyX = 0, greatX = 80, missX = 140;
 var greatNum = 0, missNum = 0;
 
-var hiraganaMap, hiragana;
 var Katakana;
